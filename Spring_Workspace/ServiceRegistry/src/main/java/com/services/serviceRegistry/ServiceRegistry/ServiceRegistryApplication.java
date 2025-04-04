0695ab5b7f219ed00b95be342002c44c85220842
ppkgit <dhsgu2017@gmail.com>
2025-04-05 01:12:44 +0530

    Just added one line comments in eureka server project

--- Spring_Workspace/ServiceRegistry/src/main/java/com/services/serviceRegistry/ServiceRegistry/ServiceRegistryApplication.java
+++ Spring_Workspace/ServiceRegistry/src/main/java/com/services/serviceRegistry/ServiceRegistry/ServiceRegistryApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//This is server spring boot project used for maintain all services of micro services
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
